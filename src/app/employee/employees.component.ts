0d8c628dc434dcf8e729e17f291868781b2bde38
yasirshabbir <yasir_shabbir@hotmail.com>
2018-07-03 21:57:14 +0800

    [ sample employees ] - employee component

---
+++ src/app/employee/employees.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit }  from '@angular/core';
+
+import { EmployeeService }    from '../employee.service';
+import { Employee }           from '../employee';
+
+@Component({
+    selector:     'app-employees',
+    templateUrl:  './employees.component.html',
+    styleUrls:    [ './employees.component.css' ]
+})
+
+export class EmployeesComponent implements OnInit {
+    employees          = [];
+    selectedEmployee   = 0;
+    title              = 'Employee List';
+
+    constructor( private employeeService: EmployeeService ) { }
+
+    ngOnInit() {
+        this.getEmployees();
+    }
+
+    getEmployees(): void {
+        this.employeeService.getEmployees()
+        .subscribe( employees => this.employees = employees );
+    }
+
+    onSelect( selectedEmployee ): void { 
+        this.selectedEmployee   = this.selectedEmployee === selectedEmployee ? null : selectedEmployee; 
+    }
+
+    onBioClicked( employee ): void {
+        alert( JSON.stringify( employee ) );
+    }
+}
